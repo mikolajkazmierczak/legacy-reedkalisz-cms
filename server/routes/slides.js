35cb7d6cd09ad598eee2f60946ac6dc4da2ab5da
Mikołaj Kaźmierczak <mikolaj@kazmierczak.pl>
2025-06-05 08:43:08 +0200

    fix: function called instead of passed in slides router

--- server/routes/slides.js
+++ server/routes/slides.js
@@ -1,11 +1,10 @@
 import auth from '#middleware/auth';
 import { crud, send } from '#middleware/crud';
 import file from '#middleware/file';
-const upload = file.save();
 
 export default (router, _) =>
   router
-    .post('/file', auth, upload, file.send)
+    .post('/file', auth, file.save, file.send)
     .put('/file', auth, file.rename)
     .delete('/file', auth, file.del)
     .post('/', auth, crud.create, send)
